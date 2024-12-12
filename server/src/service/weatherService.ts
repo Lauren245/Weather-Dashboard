8430c14b8d5861435ece1cb35f5d3fd1f77a726c
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2024-12-11 21:38:21 -0800

    started working on weatherService class. Added some links to tutorials I used to README.md.

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,14 +2,40 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
-
+interface Coordinates {
+  lon: string;
+  lat: string;
+}
 // TODO: Define a class for the Weather object
 
 // TODO: Complete the WeatherService class
-class WeatherService {
+class WeatherService implements Coordinates{
   // TODO: Define the baseURL, API key, and city name properties
+  baseURL: string;
+  APIKey: string;
+  cityName: string;
+
+  constructor(cityName: string){
+    this.baseURL = process.env.API_BASE_URL + "/data/2.5/weather?";
+    this.APIKey = process.env.API_KEY || "";
+    if(!this.APIKey){
+      throw new Error("Error: API_KEY is undefined in the .env file.");
+    };
+    this.cityName = cityName;
+  };
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+    try{
+      fetch(`${this.baseURL}q=${query}&appid=${this.APIKey}`);
+
+    }catch(error){
+      console.error("Error getting weather data for the provided location.");
+      throw new Error("Could not find weather data for the provided location.");
+    }
+      
+
+      
+  }
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
   // TODO: Create buildGeocodeQuery method
